c977f275aa471c68c29716d9ae7ed1b355b208c7
Zach Gulde <zachgulde@gmail.com>
2016-12-17 14:42:10 -0600

    build out server provisioning script and site managment script

---
+++ README.md
@@ -0,0 +1,18 @@
+# Setup Scripts
+
+For setup and managment of a remote server with tomcat and nginx.
+
+## Commands
+
+`./setup`
+
+    Setup your server for the first time
+
+`./server`
+
+    Interact with your server
+
+`./site`
+
+    Command for manipulating sites your server is setup to host
+
--- provision.sh
+++ provision.sh
@@ -13,9 +13,13 @@ heading 'installing nginx, java'
 
 apt-get install -y\
     nginx\
-    default-jdk
+    default-jdk\
+    ufw\
+    mysql-server
 
-header "Installing tomcat."
+# https://serversforhackers.com/video/installing-mysql-with-debconf
+
+heading "Installing tomcat."
 
 # download the tar from apache and extract it to /opt/tomcat
 mkdir -p /opt/tomcat
@@ -25,14 +29,50 @@ tar xzvf apache-tomcat-8.5.9.tar.gz --strip-components=1 -C /opt/tomcat
 rm apache-tomcat-8.5.9.tar.gz
 
 # create a tomcat user
-sudo groupadd tomcat
-sudo useradd -g tomcat -s /bin/false -d /opt/tomcat tomcat
+groupadd tomcat
+useradd -g tomcat -s /bin/false -d /opt/tomcat tomcat
 
 # set permissions for the tomcat install
 chown -R tomcat /opt/tomcat
+chmod a+w /opt/tomcat/webapps
 
-# disable password login + root login
-perl -i -pe 's/(PasswordAuthentication\s*)yes/\1no/' /etc/ssh/sshd_config
-perl -i -pe 's/(PermitRootLogin\s*)yes/\1no/' /etc/ssh/sshd_config
-service sshd restart
-service ssh restart
+## Here we should create the tomcat service and start it
+nohup /opt/tomcat/bin/startup.sh &
+
+## find the java installation path
+java_home=$(update-java-alternatives | awk '{print $3}')/jre
+
+# create the tomcat service
+cat > /etc/systemd/system/tomcat.service <<tomcat.service 
+[Unit]
+Description=Apache Tomcat Web Application Container
+After=network.target
+
+[Service]
+Type=forking
+
+Environment=JAVA_HOME=${java_home}
+Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat
+Environment=CATALINA_BASE=/opt/tomcat
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdown.sh
+
+User=tomcat
+Group=tomcat
+UMask=0007
+RestartSec=10
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+tomcat.service
+
+systemctl daemon-reload
+
+# start tomcat now, and have it start automatically on boot
+systemctl enable tomcat
+systemctl start tomcat
