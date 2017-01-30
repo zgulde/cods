551ebd87c25d9b26587248c00558a95e3e1d88c0
Zach Gulde <zachgulde@gmail.com>
2017-01-30 12:00:29 -0600

    setup swap file when provisioning
    java takes up a lot of memory, especially for a $5 / month droplet

--- provision.sh
+++ provision.sh
@@ -4,6 +4,13 @@ heading(){
     echo '----------------------------------'
 }
 
+# setup swap file
+fallocate -l 1G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile none swap defaults 0 0" >> /etc/fstab
+
 heading 'updating + upgrading apt'
 
 apt-get update
