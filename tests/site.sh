8b76239090f6997cf7b1f2461a34db8f53f68e85
Zach Gulde <zachgulde@gmail.com>
2018-02-11 17:04:06 -0600

    Start site testing

--- tests/site.sh
+++ tests/site.sh
@@ -1,5 +1,55 @@
-source $SCRIPTS/util.sh
+heading "Testing site commands..."
+
+logfile="$BASE_DIR/testing.log"
 
 eval "$(< $SCRIPTS/site.sh)" >/dev/null
 
-create_site
+( create_site > /dev/null ) && fail 'It should exit with a non-zero status when invoked with no arguments'
+
+echo '[TESTING] Site Creation (nginx + tomcat + git deployment)'
+echo 'Creating test site...'
+create_site --domain test.com --spring-boot --force >>$logfile 2>&1
+
+ssh $user@$ip >/dev/null <<'test'
+fail() { echo "[FAIL] $@" >&2 ; exit 1; }
+
+[[ -d /var/www/test.com/uploads ]] ||\
+	fail 'Expected an uploads directory to be created'
+stat=$(stat --format='%G:%U' /var/www/test.com/uploads)
+[[ $stat == 'tomcat:tomcat' ]] ||\
+	fail "Expected uploads directory to be owned by 'tomcat:tomcat', found '$stat'"
+[[ -f /etc/nginx/sites-available/test.com ]] ||\
+	fail 'Expected /etc/nginx/sites-available/test.com to exist'
+[[ -L /etc/nginx/sites-enabled/test.com ]] ||\
+	fail 'Expected /etc/nginx/sites-enabled/test.com to be a symlink'
+[[ $(readlink /etc/nginx/sites-enabled/test.com) == /etc/nginx/sites-available/test.com ]] ||\
+	fail 'Expected a link to the config file in sites-enabled from sites-available'
+# make sure we used the right template
+grep proxy_pass /etc/nginx/sites-available/test.com >/dev/null ||\
+	fail 'Expected to find "proxy_pass" in nginx config'
+
+sudo grep test.com /opt/tomcat/conf/server.xml >/dev/null ||\
+	fail 'Expected to find test.com in tomcat server config'
+[[ -d /opt/tomcat/test.com ]] ||\
+	fail 'Expected to find a test.com directory within the tomcat installation'
+stat=$(stat --format='%G:%U' /opt/tomcat/test.com)
+[[ $stat == 'tomcat:tomcat' ]] ||\
+	fail "Expected test.com tomcat directory to be owned by 'tomcat:tomcat', found '$stat'"
+
+[[ -d /srv/test.com ]] || fail 'Expected to find /srv/test.com directory'
+[[ -f /srv/test.com/config ]] || fail 'Expected to find a config file for test.com'
+(
+	grep '^source=application.properties$' /srv/test.com/config >/dev/null &&\
+	grep '^destination=src/main/resources/application.properties' /srv/test.com/config >/dev/null
+) || fail 'Expected to find config file pre-modified for a spring-boot app'
+
+[[ -d /srv/test.com/repo.git ]] || fail 'Expected to find a repository for test.com'
+[[ -x /srv/test.com/repo.git/hooks/post-receive ]] ||\
+	fail 'Expected to find an executable post-receive hook'
+
+test
+[[ $? -eq 0 ]] || exit 1
+
+echo 'Cleaning up...'
+remove_site --domain test.com --force >>$logfile 2>&1
+echo '[PASS]'
--- tests/util.sh
+++ tests/util.sh
@@ -6,11 +6,11 @@ valid_usernames=( zach aA1234_09 codeup a )
 invalid_usernames=( Zach '' Abcd A 'a!@#' 123 root )
 
 for username in ${valid_usernames[*]} ; do
-	valid_username $username || die "Expected username: '${username}' to be valid"
+	valid_username $username || fail "Expected username: '${username}' to be valid"
 done
 
 for username in ${invalid_usernames[*]} ; do
-	valid_username $username && die "Expected username: '${username}' to be invalid"
+	valid_username $username && fail "Expected username: '${username}' to be invalid"
 done
 
 echo '[PASS]'
@@ -19,14 +19,14 @@ echo 'testing mkpassword function...'
 
 password=$(mkpassword 4)
 if [[ ${#password} -ne 4 ]] ; then
-	die 'Expected "$(mkpassword 4)" to have a length of 4'
+	fail 'Expected "$(mkpassword 4)" to have a length of 4'
 fi
 password=$(mkpassword 24)
 if [[ ${#password} -ne 24 ]] ; then
-	die 'Expected "$(mkpassword 24)" to have a length of 24'
+	fail 'Expected "$(mkpassword 24)" to have a length of 24'
 fi
 if [[ $password =~ [^a-zA-Z0-9] ]] ; then
-	die "Found unexpected non-alphanumeric character in password: '$password'"
+	fail "Found unexpected non-alphanumeric character in password: '$password'"
 fi
 
 echo '[PASS]'
@@ -38,13 +38,13 @@ yesses=( y Y yes Yes YES )
 
 for no in ${nos[*]} ; do
 	if confirm <<< "$no" ; then
-		die "Expected confirm to return false for input '$no'"
+		fail "Expected confirm to return false for input '$no'"
 	fi
 done
 
 for yes in ${yesses[*]} ; do
 	if ! confirm <<< "$yes" ; then
-		die "Expected confirm to be true for input '$yes'"
+		fail "Expected confirm to be true for input '$yes'"
 	fi
 done
 
@@ -55,8 +55,8 @@ echo 'testing die function'
 message='some message'
 output=$(die $message)
 rc=$?
-[[ $rc -eq 0 ]] && die 'Expected die to exit with a non-zero return code'
-[[ $output == $message ]] || die 'Expected die to display passed message'
+[[ $rc -eq 0 ]] && fail 'Expected die to exit with a non-zero return code'
+[[ $output == $message ]] || fail 'Expected die to display passed message'
 
 echo '[PASS]'
 
