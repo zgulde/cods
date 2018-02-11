8931f6df8d31d8206ae9ae46b8693a8d18f7e57f
Zach Gulde <zachgulde@gmail.com>
2018-02-11 17:21:35 -0600

    Further testing for setup config files

--- tests/site.sh
+++ tests/site.sh
@@ -10,7 +10,7 @@ echo '[TESTING] Site Creation (nginx + tomcat + git deployment)'
 echo 'Creating test site...'
 create_site --domain test.com --spring-boot --force >>$logfile 2>&1
 
-ssh $user@$ip >/dev/null <<'test'
+ssh -T $user@$ip >/dev/null <<'test'
 fail() { echo "[FAIL] $@" >&2 ; exit 1; }
 
 [[ -d /var/www/test.com/uploads ]] ||\
@@ -25,8 +25,11 @@ stat=$(stat --format='%G:%U' /var/www/test.com/uploads)
 [[ $(readlink /etc/nginx/sites-enabled/test.com) == /etc/nginx/sites-available/test.com ]] ||\
 	fail 'Expected a link to the config file in sites-enabled from sites-available'
 # make sure we used the right template
-grep proxy_pass /etc/nginx/sites-available/test.com >/dev/null ||\
-	fail 'Expected to find "proxy_pass" in nginx config'
+(
+	grep proxy_pass /etc/nginx/sites-available/test.com &&\
+	egrep 'listen\s*80;' /etc/nginx/sites-available/test.com &&\
+	egrep 'server_name\s*test.com' /etc/nginx/sites-available/test.com
+) >/dev/null || fail 'Expected to nginx config to be setup correctly'
 
 sudo grep test.com /opt/tomcat/conf/server.xml >/dev/null ||\
 	fail 'Expected to find test.com in tomcat server config'
@@ -39,17 +42,23 @@ stat=$(stat --format='%G:%U' /opt/tomcat/test.com)
 [[ -d /srv/test.com ]] || fail 'Expected to find /srv/test.com directory'
 [[ -f /srv/test.com/config ]] || fail 'Expected to find a config file for test.com'
 (
-	grep '^source=application.properties$' /srv/test.com/config >/dev/null &&\
-	grep '^destination=src/main/resources/application.properties' /srv/test.com/config >/dev/null
-) || fail 'Expected to find config file pre-modified for a spring-boot app'
+	grep '^source=application.properties$' /srv/test.com/config &&\
+	grep '^destination=src/main/resources/application.properties' /srv/test.com/config
+) >/dev/null || fail 'Expected to find config file pre-modified for a spring-boot app'
 
 [[ -d /srv/test.com/repo.git ]] || fail 'Expected to find a repository for test.com'
 [[ -x /srv/test.com/repo.git/hooks/post-receive ]] ||\
 	fail 'Expected to find an executable post-receive hook'
 
 test
-[[ $? -eq 0 ]] || exit 1
 
 echo 'Cleaning up...'
 remove_site --domain test.com --force >>$logfile 2>&1
 echo '[PASS]'
+
+# echo '[TESTING] Static Site Creation'
+# echo 'Creating site...'
+# create_site --domain test.com --static --force
+# echo 'Cleaning up...'
+# remove_site --domain test.com --force >>$logfile 2>&1
+# echo '[PASS]'
