4a3a125ef491c9487373068e19ba2690cb289d2f
Zach Gulde <zachgulde@gmail.com>
2018-02-20 14:14:14 -0600

    Don't setup a swap file
    Digital ocean actually asks us not to do this
    
    see https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-16-04

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -23,13 +23,6 @@ heading(){
 
 set -e
 
-# setup swap file
-fallocate -l 1G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo "/swapfile none swap defaults 0 0" >> /etc/fstab
-
 heading 'updating + upgrading apt'
 
 apt-get update
